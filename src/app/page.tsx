593c1b0f62f0973cc85c0f5d0c174b428d4a1697
download-from-mothership <alicia@kensicomgmt.com>
2025-02-19 20:47:52 -0400

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,20 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { useCurrentUser } from '@/hooks/use-current-user';
 import LayoutSidebar from '@/components/layout-sidebar';
 import { useTranslations } from 'next-intl';
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
-export default function HomePage() {
-  const { data: currentUser, isLoading: userLoading } = useCurrentUser();
-  const t = useTranslations('home');
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
 
   return (
-    <LayoutSidebar>
-      <div className="container py-8">
-        <div className="flex flex-col gap-1">
-          <h1 className="relative text-3xl font-bold tracking-tighter sm:text-4xl">
-            <span className={userLoading ? 'opacity-0' : 'opacity-100'}>
-              {t('welcome', {
-                email: currentUser?.email || 'john.doe@example.com',
-              })}
-            </span>
-            {userLoading && <Skeleton className="absolute inset-0" />}
-          </h1>
-          <p className="text-muted-foreground">{t('description')}</p>
-        </div>
-      </div>
-    </LayoutSidebar>
-  );
+    <ul>
+      {todos?.map((todo, index) => (
+        <li key={index}>{todo}</li>
+      ))}
+    </ul>
+  )
 }
